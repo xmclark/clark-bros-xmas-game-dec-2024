efb233a6b9286ba73484911849fad7c6124f479f
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2024-12-02 21:04:42 -0800

    update player model, add test sprites, and add conveyor belt

--- Scenes/Levels/Factory/Player/player.gd
+++ Scenes/Levels/Factory/Player/player.gd
@@ -6,6 +6,8 @@ signal state_changed
 @export var SPEED: float = 100.0
 @export var JUMP_FORCE: float = 100.0
 
+@onready var animation_tree: AnimationTree = $AnimationTree
+
 const IDLE_STATE = "IdleState"
 const RUNNING_STATE = "RunningState"
 const JUMPING_STATE = "JumpingState"
@@ -13,18 +15,32 @@ const FALLING_STATE = "FallingState"
 
 var states = {}
 var current_state = null
+var last_facing_direction = Vector2(0, -1)
+var attacking = false
 
 func _ready() -> void:
-	states[IDLE_STATE] = preload("./States/idle_state.gd").new()
-	states[RUNNING_STATE] = preload("./States/running_state.gd").new()
-	states[JUMPING_STATE] = preload("./States/jumping_state.gd").new()
-	states[FALLING_STATE] = preload("./States/falling_state.gd").new()
+	states[IDLE_STATE] = $States/IdleState
+	states[RUNNING_STATE] = $States/RunningState
+	states[JUMPING_STATE] = $States/JumpingState
+	states[FALLING_STATE] = $States/FallingState
 
 	set_state(IDLE_STATE)
 
 func _physics_process(delta):
 	if current_state:
 		current_state.update(self, delta)
+	play_animation()
+	
+func play_animation():
+	var idle = !velocity
+	
+	if !idle:
+		last_facing_direction = velocity.normalized()
+
+	animation_tree.set("parameters/Idle/blend_position", last_facing_direction)
+	animation_tree.set("parameters/Run/blend_position", last_facing_direction)
+	animation_tree.set("parameters/Attack/blend_position", last_facing_direction)
+	
 
 func _input(_event):
 	var _player = self
